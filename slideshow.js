32c8adf97d38eb697cb00c8ff669debd860e8a4c
hmak5215 <hmak5215@lps53.org>
2018-01-29 13:47:53 -0600

    Slideshow

--- slideshow.js
+++ slideshow.js
@@ -1,3 +1,4 @@
+
 var slideIndex = 0;
 showSlides();
 
@@ -15,5 +16,5 @@ function showSlides() {
     }
     slides[slideIndex-1].style.display = "block";  
     dots[slideIndex-1].className += " active";
-    setTimeout(showSlides, 4000); // Change image every 4 seconds
+    setTimeout(showSlides, 4000); // Change image every 2 seconds
 }

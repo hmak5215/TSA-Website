11475c35a87341250c66c47ec35e4c0478a99784
hmak5215 <hmak5215@lps53.org>
2018-02-06 13:42:40 -0600

    Content Areas

---
+++ Test Site/boxshadow.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+
+function shadow() {
+	'use strict';
+	document.getElementById("header").style.height = document.getElementById("header_image").clientHeight;
+	document.getElementById("header").style.boxShadow
+}
